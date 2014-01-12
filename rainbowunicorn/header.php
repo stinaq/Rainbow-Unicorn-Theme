a3439bcbaf47e3cbcfe4ab60b5eb72243ef9ef1f
Stina Qvarnström <stinaqv@gmail.com>
2014-01-12 16:09:20 +0100

    Extended header

--- rainbowunicorn/header.php
+++ rainbowunicorn/header.php
@@ -8,16 +8,33 @@
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-?><!DOCTYPE html>
-<html>
-<head>
-  <title>Tutorial theme</title>
+?>
 
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-  <?php wp_head(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_title ( '|', true,'right' ); ?></title>
+     
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+     
+    <?php
+        /*
+         *  Add this to support sites with sites with threaded comments enabled.
+         */
+        if ( is_singular() && get_option( 'thread_comments' ) )
+            wp_enqueue_script( 'comment-reply' );
+     
+        wp_head();
+         
+        wp_get_archives('type=monthly&format=link');
+    ?>
 </head>
 <body>
-  <div id="wrapper">
-  <div id="header">
-  <h1>HEADER</h1>
-</div>
+ 
+<div id="wrapper">
+    <div id="header">
+        <h1><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
+    </div>
