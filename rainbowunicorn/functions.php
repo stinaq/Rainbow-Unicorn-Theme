76b32bef4b6946f080923c80b5be95c40e8d78a1
Stina Qvarnström <stinaqv@gmail.com>
2014-01-12 16:02:49 +0100

    Removes enquiring of Bootstrap CSS

--- rainbowunicorn/functions.php
+++ rainbowunicorn/functions.php
@@ -22,6 +22,3 @@ add_action( 'wp_enqueue_scripts', 'rainbowunicorn_scripts');
 
 add_action( 'wp_enqueue_scripts', 'rainbowunicorn_styles' );
 
-function rainbowunicorn_styles() {
-    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
-}
