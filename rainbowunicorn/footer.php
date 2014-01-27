6efdcf32e90ac948dba07ca34942e7cc175ef36d
Stina Qvarnström <stinaqv@gmail.com>
2014-01-27 22:46:19 +0100

    Sticky, awesome footer

--- rainbowunicorn/footer.php
+++ rainbowunicorn/footer.php
@@ -1,6 +1,7 @@
-  <footer id="footer">
-    <h1>FOOTER</h1>
-    <?php wp_footer(); ?>
+  <footer class="horizontal-centered">
+    <div class="container vertical-centered">
+      <p>hoppla</p>
+    </div>
   </footer>
 </body>
 </html>
--- rainbowunicorn/index.php
+++ rainbowunicorn/index.php
@@ -1,28 +1,27 @@
 <?php get_header(); ?>
+  <div class="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <section class="row horizontal-centered unicorn-section <?php echo unicornify(get_the_ID()); ?>">
+        <article class="container">
+          
+          <h1 class="article-headline">
+            <a href="<?php the_permalink( ) ?>">
+              <?php the_title(); ?>
+            </a>
+          </h1>
+          <h4 class="article-meta">Posted on <?php the_time('F jS, Y') ?></h4>
+          <div class="content two-columns">
+            <?php 
 
-
-    <section class="row horizontal-centered unicorn-section <?php echo unicornify(get_the_ID()); ?>">
-      <article class="container">
-        
-        <h1 class="article-headline">
-          <a href="<?php the_permalink( ) ?>">
-            <?php the_title(); ?>
-          </a>
-        </h1>
-        <h4 class="article-meta">Posted on <?php the_time('F jS, Y') ?></h4>
-        <div class="content two-columns">
-          <?php 
-
-            echo get_the_content();
-          ?>
-        </div>
-      </article>
-    </section>
+              echo get_the_content();
+            ?>
+          </div>
+        </article>
+      </section>
+      
+    <?php endwhile; else: ?>
     
-  <?php endwhile; else: ?>
-  
-  <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+  </div>
 <?php get_footer(); ?>
